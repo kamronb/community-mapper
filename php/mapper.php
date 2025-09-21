2e37d70afac39240bda3fcbf365bf698682623f4
kamronb <kamron@cmit.com.jm>
2025-09-21 01:56:16 -0500

    Added some code

--- php/mapper.php
+++ php/mapper.php
@@ -1,9 +1,26 @@
 <?php
 //Community Mapper.php
+// This script fetches the information for some polygons from an API I built
+
 echo "Hello World";
 
+// 1. I am defining the location of the api
+$apiUrl = 'https://www.bbe.com.jm/api/get-communities.php';
 
 
+//2. Create the stream contents, this is like giving instructions to file_get_contents on how to do the http request.
+
+$contextOptions = [
+	'http' => [
+		'method' => 'GET', //asking for data  from the API
+		'header' => 'User-Agent: CommunityMapperCLI/1.0\r\n', //some servers block empty User-Agents
+		'timeout' => 15 // give up if nothing is received after 15 seconds
+	],
+	'ssl' => [
+		'verify_peer' => false, //WARNING: this bypasses SSL certificate checks.
+		'verify_peer_name' => false, 
+	]
+];
 
 
 
